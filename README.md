945e11fc050656b20621980b8eb7dbfddd8f213d
Darshil Shah <shahdarshil622@gmail.com>
2021-07-09 10:59:38 +0530

    package created with greet module

---
+++ README.md
@@ -0,0 +1,4 @@
+This repository contains my own python functions and modules:
+
+REFERENCES:
+1. https://www.tutorialsteacher.com/python/python-package
---
+++ mypackages/greet.py
@@ -0,0 +1,7 @@
+def say_hello(name='World'):  #Default name is World
+	''' This function is used to greet the user'''
+	print("Hello, {}".format(name))
+	
+
+
+
