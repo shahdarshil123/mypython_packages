eeeb6d33a7caab237c707bf65c099e22798e188c
Darshil Shah <shahdarshil622@gmail.com>
2021-07-11 17:54:29 +0530

    added into README file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 This repository contains my own python functions and modules:
+
+Steps for manual installation:
+1. Go to src folder 
+2. Enter following code in command window: "python setup.py install"
--- src/setup.py
+++ src/setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 setup(name='mypackages',
-version='0.1',
+version='0.0.1',
 description='Testing installation of Package',
-url='#',
+url='https://github.com/shahdarshil123/mypython_packages',
 author='Darshil Shah',
 author_email='shahdarshil622@email.com',
 license='MIT',
